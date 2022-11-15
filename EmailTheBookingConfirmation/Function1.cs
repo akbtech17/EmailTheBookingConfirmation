e1d118c627ea0c149e8cc4a777b54953e3161468
anshul bansal <akbt.tech17@gmail.com>
2022-11-15 22:20:23 +0530

    Add project files.

---
+++ EmailTheBookingConfirmation/Function1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace EmailTheBookingConfirmation
+{
+    public class EmailTheBookingStatus
+    {
+        [FunctionName("EmailTheBookingStatus")]
+        public void Run([BlobTrigger("licenses/{name}", Connection = "BlobConnectionString")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
