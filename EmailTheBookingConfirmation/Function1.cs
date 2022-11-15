ced3438982599844a412a127ec1b3b6e0855da1b
anshul bansal <akbt.tech17@gmail.com>
2022-11-15 23:05:52 +0530

    send mail is working

--- EmailTheBookingConfirmation/Function1.cs
+++ EmailTheBookingConfirmation/Function1.cs
@@ -1,22 +1,37 @@
 using System;
 using System.IO;
+using Microsoft.Azure.KeyVault.Core;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Nest;
+using SendGrid.Helpers.Mail;
 
 namespace EmailTheBookingConfirmation
 {
     public class EmailTheBookingStatus
     {
         [FunctionName("EmailTheBookingStatus")]
-        public void Run([BlobTrigger("licenses/{name}", Connection = "BlobConnectionString")]Stream myBlob, string name, ILogger log)
+        public static void Run(
+            [BlobTrigger("licenses/{name}", Connection = "BlobConnectionString")]Stream myBlob,
+            [SendGrid(ApiKey = "SendgridAPIKey")] out SendGridMessage sendGridMessage,
+            string name, 
+            ILogger log)
         {
             try
             {
                 log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+                sendGridMessage = new SendGridMessage
+                {
+                    From = new EmailAddress("akb.tech17@gmail.com", "AzureFuncApps"),
+                };
+                sendGridMessage.AddTo("anshulkumar.bansal@lntinfotech.com");
+                sendGridMessage.SetSubject("Awesome Azure Function app");
+                sendGridMessage.AddContent("text/html", "Content");
             }
             catch (Exception ex)
             {
+                sendGridMessage = new SendGridMessage();
                 Console.WriteLine(ex.Message);
             }
             finally
