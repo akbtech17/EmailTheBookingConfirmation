7273cde60a93d5ca90a16582a8683b59a223d009
anshul bansal <akbt.tech17@gmail.com>
2022-11-16 01:28:07 +0530

    completed function app

--- EmailTheBookingConfirmation/Function1.cs
+++ EmailTheBookingConfirmation/Function1.cs
@@ -20,14 +20,81 @@ namespace EmailTheBookingConfirmation
         {
             try
             {
-                log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+                StreamReader reader = new StreamReader(myBlob); 
+                string transactionDetails = reader.ReadToEnd();
+
+                string message = GetBetween(transactionDetails, "Msssage : ", "\r\n");
+                string email = GetBetween(transactionDetails, "Email : ", "\r\n");
+
                 sendGridMessage = new SendGridMessage
                 {
-                    From = new EmailAddress("akb.tech17@gmail.com", "AzureFuncApps"),
+                    From = new EmailAddress("akb.tech17@gmail.com", "Book My Movie"),
                 };
-                sendGridMessage.AddTo("anshulkumar.bansal@lntinfotech.com");
-                sendGridMessage.SetSubject("Awesome Azure Function app");
-                sendGridMessage.AddContent("text/html", "Content");
+                sendGridMessage.AddTo(email);
+
+                if (message.Equals("BCNF"))
+                {
+                    string customerName = GetBetween(transactionDetails, "Name : ", "\r\n");
+                    string movieName = GetBetween(transactionDetails, "Movie name : ", "\r\n");
+                    string noOfTicketsBooked = GetBetween(transactionDetails, "Number of tickets booked : ", "\r\n");
+                    string seatNos = GetBetween(transactionDetails, "Seat numbers : ", "\r\n");
+                    string totalCost = GetBetween(transactionDetails, "Total cost : ", "\r\n");
+                    string transactionTime = GetBetween(transactionDetails, "Transaction time : ", "\r\n");
+                    string showTime = GetBetween(transactionDetails, "Show time : ", "\r\n");
+
+                    
+                    sendGridMessage.SetSubject("Your booking is Confirmed!");
+
+                    string htmlContent = "" +
+                        $"<h1>Thankyou {customerName}</h1>" +
+                        $"<h3>for booking ticket from book my movie</h3>" +
+                        $"<hr/>" +
+                        $"<br>" +
+                        $"<h2>Movie Name : {movieName}</h2>" +
+                        $"<h2>Show Time :  {showTime}</h2>" +
+                        $"<h3>No of Seats Booked :  {noOfTicketsBooked}</h3>" +
+                        $"<h3>Seat Numbers :  {seatNos}</h3>" +
+                        $"<h4>Venue : Cinepolis: WESTEND Mall Aundh, Pune(AUDI 03) Pune, Pune</h4>" +
+                        $"<hr/>" +
+                        $"<h5>Transaction Time : {transactionTime}</h5>" +
+                        $"<h5>Total Cost : {totalCost}</h5>";
+                      
+                    sendGridMessage.AddContent("text/html", htmlContent);
+                }
+                else 
+                {
+                    string customerName = GetBetween(transactionDetails, "Name : ", "\r\n");
+                    string movieName = GetBetween(transactionDetails, "Movie name : ", "\r\n");
+                    string noOfTicketsCancelled = GetBetween(transactionDetails, "Number of tickets cancelled : ", "\r\n");
+                    string seatNos = GetBetween(transactionDetails, "Seat numbers : ", "\r\n");
+                    string refundCost = GetBetween(transactionDetails, "Refund cost : ", "\r\n");
+                    string transactionTime = GetBetween(transactionDetails, "Transaction time : ", "\r\n");
+                    string showTime = GetBetween(transactionDetails, "Show time : ", "\r\n");
+
+                    sendGridMessage.SetSubject("Your booking is Cancelled!");
+
+                    string htmlContent = "" +
+                        $"<h1>Thankyou {customerName}</h1>" +
+                        $"<h3>for using book my movie</h3>" +
+                        $"<hr/>" +
+                        $"<br>" +
+                        $"<h2>Movie Name : {movieName}</h2>" +
+                        $"<h2>Show Time :  {showTime}</h2>" +
+                        $"<h3>No of Seats Cancelled :  {noOfTicketsCancelled}</h3>" +
+                        $"<h3>Seat Numbers :  {seatNos}</h3>" +
+                        $"<h4>Venue : Cinepolis: WESTEND Mall Aundh, Pune(AUDI 03) Pune, Pune</h4>" +
+                        $"<hr/>" +
+                        $"<h5>Cancellation Time : {transactionTime}</h5>" +
+                        $"<h5>Refund Cost : {refundCost}</h5>";
+
+                    sendGridMessage.AddContent("text/html", htmlContent);
+                }
+
+                
+
+ 
+
+                log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
             }
             catch (Exception ex)
             {
@@ -38,6 +105,26 @@ namespace EmailTheBookingConfirmation
             { 
                 myBlob.Close();
             }
+        }
+
+        public static string GetBetween(string strSource, string strStart, string strEnd)
+        {
+            if (strEnd.Equals(""))
+            {
+                int start, end;
+                start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                end = strSource.Length;
+                return strSource.Substring(start, end - start);
+            }
+            else if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                int start, end;
+                start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                end = strSource.IndexOf(strEnd, start);
+                return strSource.Substring(start, end - start);
+            }
+
+            return "";
         }
     }
 }
